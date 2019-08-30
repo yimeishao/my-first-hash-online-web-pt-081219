83a3cc70fb856346fdf5a518b4657e81ffb2f2e3
Maria Yimei Shao <maria.yimei.shao@gmail.com>
2019-08-30 23:53:28 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -13,9 +13,7 @@ def retrieval
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+ shipping_manifest["oil paintings"]
 end
 
 def adding
@@ -25,13 +23,13 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
-
+shipping_manifest << "muskets => 2"
 
 
   # add 4 gun powder to the shipping_manifest hash below
-
+shipping_manifest << "gun powder" => 4
 
 
   # return the shipping_manifest hash below
-
+shipping_manifest
 end
